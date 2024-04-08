3488b55dcb6b3a28570276fe73a33dccc294aa97
Mdhamedd <mohamedhamed152001@gmail.com>
2024-04-08 17:45:02 +0200

    new style

---
+++ AddPage/assets/main.js
@@ -0,0 +1,11 @@
+var form = document.querySelector(".Signin__form");
+
+// Create a new div element
+var div = document.createElement("div");
+
+// Set the content of the div to a plus sign
+div.textContent = "+";
+
+// Style the div to be centered
+div.style.textAlign = "center";
+div.style.fontSize = "2em"; //
